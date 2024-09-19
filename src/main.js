020d92756c283c0ac35f5ab0b0e3a25ceadce284
鹊楠 <11601350+quenan666@user.noreply.gitee.com>
2024-09-19 20:02:08 +0800

    perf: ⚡ 组件库按需引入，优化加载速度

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import Vuesax from 'vuesax-alpha'
+import { VsButton, VsAvatar, VsTooltip, VsDialog, VsNotification, VsAlert } from 'vuesax-alpha'
 import VueTyped from 'vue3-typed-js'
 import { MotionPlugin } from '@vueuse/motion'
 
@@ -12,7 +12,7 @@ import 'vuesax-alpha/dist/index.css'
 import 'vuesax-alpha/theme-chalk/dark/css-vars.css'
 
 const app = createApp(App)
-app.use(Vuesax)
+app.use(VsButton).use(VsAvatar).use(VsTooltip).use(VsDialog).use(VsNotification).use(VsAlert)
 app.use(VueTyped)
 app.use(MotionPlugin)
 app.mount('#app')
